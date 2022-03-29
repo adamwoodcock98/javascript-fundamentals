b26224dce090e09a770abb7a309d3a18161a2b75
Adam Woodcock <adam.woodcock98@gmail.com>
2022-03-29 16:04:49 +0100

    Searching an array of objects

--- candies.js
+++ candies.js
@@ -1,42 +1,71 @@
-const addToBatch = (arr, num) => {
-  if (arr.length >= 5) {
-    return arr;
-  } else {
-    return arr.concat(num);
-  }
-}
+// const addToBatch = (arr, num) => {
+//   if (arr.length >= 5) {
+//     return arr;
+//   } else {
+//     return arr.concat(num);
+//   }
+// }
 
-module.exports = addToBatch;
+// module.exports = addToBatch;
 
-const checkLength = (numberString) => {
-  return numberString.length <= 10;
-}
+// const checkLength = (numberString) => {
+//   return numberString.length <= 10;
+// }
 
-const filterLongNumbers = (numberArray) => {
-  return numberArray.filter(checkLength);
-}
+// const filterLongNumbers = (numberArray) => {
+//   return numberArray.filter(checkLength);
+// }
 
-const numbers = [
-  '1763687364',
-  '4763687363',
-  '7867867862',
-  'aaaaaaaabbbbbbbcccccdddddddd'
+// const numbers = [
+//   '1763687364',
+//   '4763687363',
+//   '7867867862',
+//   'aaaaaaaabbbbbbbcccccdddddddd'
+// ];
+
+// const generateMessages = (namesAndDiscounts) => {
+//   return namesAndDiscounts.map(item => {
+//     const name = item.name;
+//     const discount = item.discount;
+//     return `Hi ${name}! ${discount}% off our best candies for you today`;
+//   });
+// }
+
+// const namesAndDiscounts = [
+//   { name: 'Anna', discount: 50 },
+//   { name: 'Laura', discount: 40 },
+//   { name: 'Josh', discount: 30 },
+//   { name: 'Min', discount: 50 },
+//   { name: 'Karla',discount:  6 }
+// ];
+
+// console.log(generateMessages(namesAndDiscounts))
+
+const candies = [
+  { name: 'Aero', price: 1.99 },
+  { name: 'Skitties', price: 2.99 },
+  { name: 'Mars', price: 1.49 },
+  { name: 'Maltesers', price: 3.49 },
+  { name: 'Skittles', price: 1.49 },
+  { name: 'Starburst', price: 5.99 },
+  { name: 'Ricola', price: 1.99 },
+  { name: 'Polkagris', price: 5.99 },
+  { name: 'Pastila', price: 4.99 },
+  { name: 'Mentos', price: 8.99 },
+  { name: 'Raffaello', price: 7.99 },
+  { name: 'Gummi bears', price: 10.99 },
+  { name: 'Fraise Tagada', price: 5.99 }
 ];
 
-const generateMessages = (namesAndDiscounts) => {
-  return namesAndDiscounts.map(item => {
-    const name = item.name;
-    const discount = item.discount;
-    return `Hi ${name}! ${discount}% off our best candies for you today`;
-  });
+const searchCandies = (str, cost) => {
+  return candies
+    .filter(item => item.price <= cost)
+    .filter(item => item.name.toLowerCase().startsWith(str.toLowerCase()))
+    .map(item => item.name);
 }
 
-const namesAndDiscounts = [
-  { name: 'Anna', discount: 50 },
-  { name: 'Laura', discount: 40 },
-  { name: 'Josh', discount: 30 },
-  { name: 'Min', discount: 50 },
-  { name: 'Karla',discount:  6 }
-];
+const checkPrice = (maxPrice) => {
+  return 
+}
 
-console.log(generateMessages(namesAndDiscounts))
+module.exports = searchCandies;
--- candies.test.js
+++ candies.test.js
@@ -1,2 +1,23 @@
-const addToBatch = require('./candies')
+const searchCandies = require('./candies')
 
+describe('Searching Candies', () => {
+  it('should return Mars and Maltesers', () => {
+    expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers']);
+  })
+
+  it('should return Mars', () => {
+    expect(searchCandies('Ma', 2)).toEqual(['Mars']);
+  })
+
+  it('should return Skitties, Skittles, Starburst', () => {
+    expect(searchCandies('S', 10)).toEqual(['Skitties', 'Skittles', 'Starburst']);
+  })
+
+  it('should return Skitties and Skittles', () => {
+    expect(searchCandies('S', 4)).toEqual(['Skitties', 'Skittles']);
+  })
+
+  it('should return successfully when passed a lowercase inpu', () => {
+    expect(searchCandies('s', 4)).toEqual(['Skitties', 'Skittles']);
+  })
+})
